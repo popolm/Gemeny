8d92a147db2a64730ec0ace44c0e62a644a1c5e9
Pauline <lmg.pauline1@gmail.com>
2025-05-04 14:16:20 +0200

    modif

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 const messages = [];
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Backend en ligne sur http://localhost:${PORT}`);});
+  console.log(`✅! Backend en ligne sur http://localhost:${PORT}`);});
 
 
 app.use(
