38a9ebf358ecc11ad9e450a7734936c807f75a9a
Pauline <lmg.pauline1@gmail.com>
2025-05-04 13:21:22 +0200

    modif

--- backend/test/load/load_test.js
+++ backend/test/load/load_test.js
@@ -1,11 +1,14 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-export let options = {
+export const options = {
   vus: 100,
   duration: "30s",
+  thresholds: {
+    http_req_duration: ["p(95)<500"], // 95% des requêtes < 500ms
+    http_req_failed: ["rate<0.01"], // <1% d'erreurs
+  },
 };
-
 export default function () {
   const res = http.get("http://localhost:81/hello");
   console.log(res.body);
